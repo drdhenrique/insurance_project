bfe2200278f37a5d79a4a705b425dfdacd39c180
Eduardo Henrique <drdhenri@gmail.com>
2024-04-25 22:42:34 -0300

    raw basic description by describe()

--- notebooks/1.0_eh_initial_data_explorations.py
+++ notebooks/1.0_eh_initial_data_explorations.py
@@ -1,16 +1,55 @@
 # %%
 
+# imports
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import math
+from IPython.core.display import HTML
+from sklearn.model_selection import train_test_split
 
 # %%
 
-df_raw = pd.read_csv("../data/raw/test.csv", index_col=[0])
+# auxiliary functions
+
+%matplotlib inline
+
+plt.style.use( 'bmh' )
+plt.rcParams['figure.figsize'] = [25, 12]
+plt.rcParams['font.size'] = 24
+display( HTML( '<style>.container { width:100% !important; }</style>') )
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
+pd.set_option( 'display.expand_frame_repr', False )
+sns.set_theme(style="ticks", color_codes=True)
 
 
 # %%
-df = df_raw.sample(5000, random_state=31415)
+
+# data import 
+# sampling a subset to work on
+# save the information .describe() from raw_data
+
+
+df_raw = pd.read_csv("../data/raw/train.csv", index_col=[0])
+
+X = df_raw.drop('target', axis = 1)
+y = df_raw['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size= 0.2, stratify= y)
+
+desc_raw_data = df_raw.describe()
+
+desc_raw_data.to_csv('../data/interim/desc_raw_data')
+
+
+
+
 # %%
+df = pd.read_csv('../data/interim/df.csv', index_col=[0])
+df.head()
+# %%
+
+1/(y_train.value_counts()[0]/y_train.value_counts()[1])
