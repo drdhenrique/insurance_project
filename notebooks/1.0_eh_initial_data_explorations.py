40a7d204f52f6d40f496dcef0a06cb2351c9093a
Eduardo Henrique <drdhenri@gmail.com>
2024-05-05 00:08:55 -0300

    counted missing values

--- notebooks/1.0_eh_initial_data_explorations.py
+++ notebooks/1.0_eh_initial_data_explorations.py
@@ -33,11 +33,27 @@ df_raw = pd.read_csv("../data/raw/train.csv", index_col=[0])
 # %%
 
 # is there any NA?
-df_raw.isna().sum()
+df_raw.isna().sum().sum()
 
-# nop! 
+# nop! But in the data description on kaggle, missing are assigned to -1, so further exploration needed!
+
 # %%
+list(df_raw.columns) 
 
+# %%
+cond1 = df_raw['target'] == -1
+sum(cond1)
+
+# %%
+# counting the -1 values
+
+for i in list(df_raw.columns):
+    cond1 = df_raw[i] == -1
+
+    if sum(cond1) != 0:
+        print(f'The total of -1 in the "{i}" column is', df_raw[cond1].shape[0]) 
+        
+# %%
 # description of raw data
 
 df_raw.describe(include='all')
@@ -46,3 +62,27 @@ df_raw.describe(include='all')
 # description of raw data
 
 df_raw.info()
+# %%
+
+# there's four variable groups:
+# ind - individual
+# reg - region
+# car - car :D
+# calc - a calculated feature
+
+# there's also some identifiers:
+# bin - binary variable
+# cat - categorical variable
+
+df_car = df_raw[[x for x in df_raw.columns if 'car' in x]]
+df_calc = df_raw[[x for x in df_raw.columns if 'calc' in x]]
+df_reg = df_raw[[x for x in df_raw.columns if 'reg' in x]]
+df_ind = df_raw[[x for x in df_raw.columns if 'ind' in x]]
+df_bin = df_raw[[x for x in df_raw.columns if 'bin' in x]]
+df_cat = df_raw[[x for x in df_raw.columns if 'cat' in x]]
+
+
+
+# %%
+
+sns.barplot(df_bin.ps_ind_18_bin)
